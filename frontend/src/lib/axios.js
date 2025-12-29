c79468e6ad263b906c94593ead76ca89c4c4e87e
Ankush Kumar Yadav <143339743+Ankush-Kumar-Yadav@users.noreply.github.com>
2025-12-29 16:22:45 +0530

    Update axios base URL for development mode

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "https://e-commerce-backend-o7e0.onrender.com/api" : "/api",
+	baseURL: import.meta.mode === "development" ? "https://e-commerce-cib8.onrender.com/api" : "/api",
 	withCredentials: true, // send cookies to the server
 });
 
